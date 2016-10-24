121dcb9408e886f4620f5f60d4370d5dfa71032c
emibia <emidiobia@gmail.com>
2016-10-24 01:22:11 +0100

    Add Polymer library and elements

---
+++ app/main.js
@@ -0,0 +1,24 @@
+(function () {'use strict';
+
+const task_1 = require("./task");
+let taskList = new Array();
+let t1 = new task_1.Task();
+t1.title = "Task 1";
+let t2 = new task_1.Task();
+t2.title = "Task 2";
+let t3 = new task_1.Task();
+t3.title = "Task 3";
+taskList.push(t1);
+taskList.push(t2);
+taskList.push(t3);
+console.log(taskList);
+document.addEventListener('WebComponentsReady', function () {
+    var input = document.querySelector('paper-input');
+    var button = document.querySelector('paper-button');
+    var greeting = document.getElementById("greeting");
+    button.addEventListener('click', function () {
+        greeting.textContent = 'Hello, ' + input.value;
+    });
+});
+}());
+//# sourceMappingURL=main.js.map
---
+++ src/user.js
@@ -0,0 +1,23 @@
+"use strict";
+class User {
+    constructor(firstName, lastName) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+    get fullName() {
+        return this._firstName + ' ' + this._lastName;
+    }
+    get firstName() {
+        return this._firstName;
+    }
+    get lastName() {
+        return this._lastName;
+    }
+    set firstName(newName) {
+        this._firstName = newName;
+    }
+    set lastName(newName) {
+        this._lastName = newName;
+    }
+}
+exports.User = User;
